beb575995151db4d2034913e6bcf2a0b1a6fd85d
mrybak834 <mrybak834@gmail.com>
2016-04-09 23:54:08 -0500

    Finished project (WITH ERRORS)

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,5 @@
-import java.net.*; 
-import java.io.*; 
+import java.net.*;
+import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Vector;
@@ -7,137 +7,175 @@ import javax.swing.*;
 
 import static java.lang.Thread.sleep;
 
+
+//TODO
+
+/**
+ * 1. Select who to chat with
+ * 2. Refresh the list of clients (add and delete)
+ * 3. Check client disconnects properly
+ * 4. Fix UI
+ * 5. No ~ or , or ! in username or private message names
+ */
+
 public class Client extends JFrame implements ActionListener
-{  
-  // GUI items
-  JButton sendButton;
-  JButton connectButton;
-  JTextField machineInfo;
-  JTextField portInfo;
-  JTextField message;
-  JTextArea history;
-  JTextArea people;
+{
+    // GUI items
+    JButton sendButton;
+    JButton connectButton;
+    JTextField machineInfo;
+    JTextField portInfo;
+    JTextField message;
+    JTextArea history;
+    JTextArea people;
     JTextField username;
+    JTextField sendToNames;
 
-  // Network Items
-  boolean connected;
-  Socket echoSocket;
-  PrintWriter out;
-  BufferedReader in;
+    // Network Items
+    boolean connected;
+    Socket echoSocket;
+    PrintWriter out;
+    BufferedReader in;
 
-   // set up GUI
-   public Client()
-   {
-      super( "Echo Client" );
+    // set up GUI
+    public Client()
+    {
+        super( "Echo Client" );
 
-      // get content pane and set its layout
-      Container container = getContentPane();
-      container.setLayout (new BorderLayout ());
-      
-      // set up the North panel
-      JPanel upperPanel = new JPanel ();
-      upperPanel.setLayout (new GridLayout (5,2));
-      container.add (upperPanel, BorderLayout.NORTH);
-      
-      // create buttons
-      connected = false;
+        // get content pane and set its layout
+        Container container = getContentPane();
+        container.setLayout (new BorderLayout ());
 
-      upperPanel.add ( new JLabel ("Message: ", JLabel.RIGHT) );
-      message = new JTextField ("");
-      message.addActionListener( this );
-      upperPanel.add( message );
-      
-      sendButton = new JButton( "Send Message" );
-      sendButton.addActionListener( this );
-      sendButton.setEnabled (false);
-      upperPanel.add( sendButton );
+        // set up the North panel
+        JPanel upperPanel = new JPanel ();
+        upperPanel.setLayout (new GridLayout (6,2));
+        container.add (upperPanel, BorderLayout.NORTH);
 
-      connectButton = new JButton( "Connect to Server" );
-      connectButton.addActionListener( this );
-      upperPanel.add( connectButton );
-                      
-      upperPanel.add ( new JLabel ("Server Address: ", JLabel.RIGHT) );
-      machineInfo = new JTextField ("127.0.0.1");
-      upperPanel.add( machineInfo );
-                      
-      upperPanel.add ( new JLabel ("Server Port: ", JLabel.RIGHT) );
-      portInfo = new JTextField ("");
-      upperPanel.add( portInfo );
-                      
-      history = new JTextArea ( 10, 35 );
-      history.setEditable(false);
-      container.add( new JScrollPane(history) ,  BorderLayout.EAST);
-      
-      people = new JTextArea ( 10, 25 );
-      people.setEditable(false);
-      people.append("Clients connected to the server:\n");
-      container.add( new JScrollPane(people) ,  BorderLayout.WEST);
+        // create buttons
+        connected = false;
 
-       upperPanel.add ( new JLabel ("Username: ", JLabel.RIGHT) );
-       username = new JTextField ("");
-       username.addActionListener( this );
-       upperPanel.add( username );
+        upperPanel.add ( new JLabel ("Message: ", JLabel.RIGHT) );
+        message = new JTextField ("");
+        message.addActionListener( this );
+        upperPanel.add( message );
 
-      setSize( 800, 400 );
-      setVisible( true );
+        sendButton = new JButton( "Send Message" );
+        sendButton.addActionListener( this );
+        sendButton.setEnabled (false);
+        upperPanel.add( sendButton );
 
-   } // end CountDown constructor
+        connectButton = new JButton( "Connect to Server" );
+        connectButton.addActionListener( this );
+        upperPanel.add( connectButton );
 
-   public static void main( String args[] )
-   { 
-       final Client application = new Client();
+        upperPanel.add ( new JLabel ("Server Address: ", JLabel.RIGHT) );
+        machineInfo = new JTextField ("127.0.0.1");
+        upperPanel.add( machineInfo );
 
-       //If the client exits, make sure to disconnect them as well
-       application.addWindowListener(new WindowAdapter() {
-           @Override
-           public void windowClosing(WindowEvent e) {
-               try {
-                   System.out.println("CLOSING");
+        upperPanel.add ( new JLabel ("Server Port: ", JLabel.RIGHT) );
+        portInfo = new JTextField ("");
+        upperPanel.add( portInfo );
 
-                   if(application.echoSocket != null && application.echoSocket.isConnected()) {
-                       //Tell the server to decrease the vector of sockets
-                       //TODO
+        history = new JTextArea ( 10, 35 );
+        history.setEditable(false);
+        container.add( new JScrollPane(history) ,  BorderLayout.EAST);
+
+        people = new JTextArea ( 10, 25 );
+        people.setEditable(false);
+        people.append("Clients connected to the server:\n");
+        container.add( new JScrollPane(people) ,  BorderLayout.WEST);
+
+        upperPanel.add ( new JLabel ("Username: ", JLabel.RIGHT) );
+        username = new JTextField ("");
+        username.addActionListener( this );
+        upperPanel.add( username );
 
 
-                       application.echoSocket.close();
-                   }
-               } catch (IOException e1) {
-                   e1.printStackTrace();
-               }
-           }
-       });
+        upperPanel.add ( new JLabel ("Send to: ", JLabel.RIGHT) );
+        sendToNames = new JTextField ("");
+        sendToNames.addActionListener( this );
+        upperPanel.add( sendToNames );
 
-       application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-   }
+        setSize( 800, 400 );
+        setVisible( true );
+
+    } // end CountDown constructor
+
+    public static void main( String args[] )
+    {
+        final Client application = new Client();
+
+        //If the client exits, make sure to disconnect them as well
+        application.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                try {
+                    System.out.println("CLOSING");
+
+                    if(application.echoSocket != null && application.echoSocket.isConnected()) {
+                        //Tell the server to decrease the vector of sockets
+                        //TODO
+
+
+                        application.echoSocket.close();
+                    }
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    }
 
     // handle button event
     public void actionPerformed( ActionEvent event )
     {
-       if ( connected && 
-           (event.getSource() == sendButton || 
-            event.getSource() == message ) )
-       {
-         doSendMessage();
-       }
-       else if (event.getSource() == connectButton)
-       {
-         doManageConnection();
-       }
+        if ( connected &&
+                (event.getSource() == sendButton ||
+                        event.getSource() == message ) )
+        {
+            doSendMessage();
+        }
+        else if (event.getSource() == connectButton)
+        {
+            doManageConnection();
+        }
     }
 
     public void doSendMessage()
     {
-      try
-      {
+        try
+        {
 
-              out.println(username.getText() + ": " + message.getText());
-              //history.insert(in.readLine() + "\n", 0);
+            //Global message
+            if(sendToNames.getText().equals("")){
+                out.println(username.getText() + ": " + message.getText());
+            }
+            //Private message
+            else{
+                out.println("!" + sendToNames.getText() + "," + username.getText() + "~" + username.getText() + ": " + message.getText());
+            }
 
 
-      }
-      catch (Exception e) {
-          history.insert("Error in processing message ", 0);
-      }
+        }
+        catch (Exception e) {
+            history.insert("Error in processing message ", 0);
+        }
+    }
+
+    //Send a false message in order for the server to process and return the list of clients
+    public void doSendActivationMessage()
+    {
+        try
+        {
+
+            out.println(" " + " " + " " );
+
+        }
+        catch (Exception e) {
+            history.insert("Error in processing message ", 0);
+        }
     }
 
 
@@ -147,13 +185,78 @@ public class Client extends JFrame implements ActionListener
         public messageReceiverThread(BufferedReader in){
             this.in = in;
         }
+
         public void run() {
+            int i = 0;
+            String[] line = new String[10];
+            int count = 0;
+
             while(true){
+
                 try {
+
                     while(in.ready()) {
-                        String line;
-                        line = in.readLine();
-                        history.insert(line + "\n", 0);
+                        line[i] = in.readLine();
+                        char c = line[i].charAt(0);
+
+                        if( ((int) c > 47) && ((int) c < 58) )
+                        {
+                            people.setText("");
+                            people.append("Clients connected to the server: \n");
+
+                            for(int k = 0; k < i; k++)
+                            {
+                                if(line[i].equals(line[k]))
+                                    count++;
+                            }
+
+                            if(count == 0)
+                            {
+                                i++;
+                            }
+                            line[i] = "";
+
+
+                            for(int j = 0;j < 10; j ++)
+                            {
+                                if(line[j] != null)
+                                    people.append(line[j] + "\n");
+
+                            }
+
+                        }
+                        //Print message to chat pane
+                        else {
+                            //Check if the message is meant for the user
+                            if(line[i].charAt(0) == '!'){
+                                //Get the list of names
+                                String userString = line[i];
+                                String nameSubstring = line[i].substring(userString.indexOf('!') + 1, userString.indexOf('~'));
+
+                                //Get the string to be displayed
+                                String displayString = line[i].substring(userString.indexOf('~')+1);
+
+                                //Split the names up, ignoring commas and whitespace
+                                String[] names = nameSubstring.split("\\s*,\\s*");
+
+
+                                //TODO
+                                //Blank out repeated names
+
+
+                                for(String s: names){
+                                    //If the string was sent to us, display
+                                    if(username.getText().equals(s)){
+                                        history.insert("PRIVATE: " + displayString + "\n", 0);
+                                    }
+                                }
+
+                            }
+                            else{
+                                history.insert(line[i] + "\n", 0);
+                            }
+                        }
+
                     }
 
                     //System.out.println(line);
@@ -168,52 +271,56 @@ public class Client extends JFrame implements ActionListener
 
     public void doManageConnection()
     {
-      if (connected == false)
-      {
-        String machineName = null;
-        int portNum = -1;
-        try {
-            machineName = machineInfo.getText();
-            portNum = Integer.parseInt(portInfo.getText());
-            echoSocket = new Socket(machineName, portNum );
-            out = new PrintWriter(echoSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(
-                                        echoSocket.getInputStream()));
-            sendButton.setEnabled(true);
-            connected = true;
-            connectButton.setText("Disconnect from Server");
-
-            messageReceiverThread t = new messageReceiverThread(in);
-            t.start();
-
-        } catch (NumberFormatException e) {
-            history.insert ( "Server Port must be an integer\n", 0);
-        } catch (UnknownHostException e) {
-            history.insert("Don't know about host: " + machineName , 0);
-        } catch (IOException e) {
-            history.insert ("Couldn't get I/O for "
-                               + "the connection to: " + machineName , 0);
-        }
-
-      }
-      else
-      {
-        try 
+        if (connected == false)
         {
-          out.close();
-          in.close();
-          echoSocket.close();
-          sendButton.setEnabled(false);
-          connected = false;
-          connectButton.setText("Connect to Server");
-        }
-        catch (IOException e) 
-        {
-            history.insert ("Error in closing down Socket ", 0);
-        }
-      }
+            String machineName = null;
+            int portNum = -1;
+            try {
+                machineName = machineInfo.getText();
+                portNum = Integer.parseInt(portInfo.getText());
+                echoSocket = new Socket(machineName, portNum );
+                out = new PrintWriter(echoSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(
+                        echoSocket.getInputStream()));
+                sendButton.setEnabled(true);
+                connected = true;
+                connectButton.setText("Disconnect from Server");
 
-        
+                //Start listening for messages from the server
+                messageReceiverThread t = new messageReceiverThread(in);
+                t.start();
+
+                //Get the list of clients
+                doSendActivationMessage();
+
+            } catch (NumberFormatException e) {
+                history.insert ( "Server Port must be an integer\n", 0);
+            } catch (UnknownHostException e) {
+                history.insert("Don't know about host: " + machineName , 0);
+            } catch (IOException e) {
+                history.insert ("Couldn't get I/O for "
+                        + "the connection to: " + machineName , 0);
+            }
+
+        }
+        else
+        {
+            try
+            {
+                out.close();
+                in.close();
+                echoSocket.close();
+                sendButton.setEnabled(false);
+                connected = false;
+                connectButton.setText("Connect to Server");
+            }
+            catch (IOException e)
+            {
+                history.insert ("Error in closing down Socket ", 0);
+            }
+        }
+
+
     }
 
- } // end class EchoServer3
+} // end class EchoServer3
--- src/Server.java
+++ src/Server.java
@@ -1,173 +1,173 @@
-import java.net.*; 
-import java.io.*; 
+import java.net.*;
+import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Vector;
 import javax.swing.*;
 
 public class Server extends JFrame implements ActionListener{
-  
-  // GUI items
-  JButton ssButton;
-  JLabel machineInfo;
-  JLabel portInfo;
-  JTextArea history;
-  JTextArea people;
-  private boolean running;
 
-  // Network Items
-  boolean serverContinue;
-  ServerSocket serverSocket;
+    // GUI items
+    JButton ssButton;
+    JLabel machineInfo;
+    JLabel portInfo;
+    JTextArea history;
+    private boolean running;
+
+    // Network Items
+    boolean serverContinue;
+    ServerSocket serverSocket;
     public Vector<Socket> socketVector;
 
-   // set up GUI
-   public Server()
-   {
-      super( "Echo Server" );
+    // set up GUI
+    public Server()
+    {
+        super( "Echo Server" );
 
-      // get content pane and set its layout
-      Container container = getContentPane();
-      container.setLayout( new FlowLayout() );
+        // get content pane and set its layout
+        Container container = getContentPane();
+        container.setLayout( new FlowLayout() );
 
-      // create buttons
-      running = false;
-      ssButton = new JButton( "Start Listening" );
-      ssButton.addActionListener( this );
-      container.add( ssButton );
+        // create buttons
+        running = false;
+        ssButton = new JButton( "Start Listening" );
+        ssButton.addActionListener( this );
+        container.add( ssButton );
 
-      String machineAddress = null;
-      try
-      {  
-        InetAddress addr = InetAddress.getLocalHost();
-        machineAddress = addr.getHostAddress();
-      }
-      catch (UnknownHostException e)
-      {
-        machineAddress = "127.0.0.1";
-      }
-      machineInfo = new JLabel (machineAddress);
-      container.add( machineInfo );
-      portInfo = new JLabel (" Not Listening ");
-      container.add( portInfo );
+        String machineAddress = null;
+        try
+        {
+            InetAddress addr = InetAddress.getLocalHost();
+            machineAddress = addr.getHostAddress();
+        }
+        catch (UnknownHostException e)
+        {
+            machineAddress = "127.0.0.1";
+        }
+        machineInfo = new JLabel (machineAddress);
+        container.add( machineInfo );
+        portInfo = new JLabel (" Not Listening ");
+        container.add( portInfo );
 
-      history = new JTextArea ( 10, 40 );
-      history.setEditable(false);
-      container.add( new JScrollPane(history) );
-      
-      people = new JTextArea ( 10, 25 );
-      people.setEditable(false);
-      people.append("Clients connected to the server:\n");
-      container.add( new JScrollPane(people) );
+        history = new JTextArea ( 10, 40 );
+        history.setEditable(false);
+        container.add( new JScrollPane(history) );
 
-      setSize( 500, 420 );
-      setVisible( true );
+        setSize( 500, 420 );
+        setVisible( true );
 
-   } // end CountDown constructor
+    } // end CountDown constructor
 
-   public static void main( String args[] )
-   { 
-      Server application = new Server();
-      application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-   }
+    public static void main( String args[] )
+    {
+        Server application = new Server();
+        application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    }
 
     // handle button event
     public void actionPerformed( ActionEvent event )
     {
-       if (running == false)
-       {
-         new ConnectionThread (this);
-       }
-       else
-       {
-         serverContinue = false;
-         ssButton.setText ("Start Listening");
-         portInfo.setText (" Not Listening ");
-       }
+        if (running == false)
+        {
+            new ConnectionThread (this);
+        }
+        else
+        {
+            serverContinue = false;
+            ssButton.setText ("Start Listening");
+            portInfo.setText (" Not Listening ");
+        }
     }
 
 
- } // end class EchoServer3
+} // end class EchoServer3
 
 
 class ConnectionThread extends Thread
- {
-   Server gui;
-     public Vector<PrintWriter> socketVector;
-   
-   public ConnectionThread (Server es3)
-   {
-     gui = es3;
-       socketVector = new Vector<>();
-     start();
-   }
-   
-   public void run()
-   {
-     gui.serverContinue = true;
-     
-     try 
-     { 
-       gui.serverSocket = new ServerSocket(0); 
-       gui.portInfo.setText("Listening on Port: " + gui.serverSocket.getLocalPort());
-       System.out.println ("Connection Socket Created");
-       try { 
-         while (gui.serverContinue)
-         {
-           System.out.println ("Waiting for Connection");
-           gui.ssButton.setText("Stop Listening");
-           CommunicationThread thread = new CommunicationThread (gui.serverSocket.accept(), gui, socketVector);
-             thread.start();
-         }
-       } 
-       catch (IOException e) 
-       { 
-         System.err.println("Accept failed."); 
-         System.exit(1); 
-       } 
-     } 
-     catch (IOException e) 
-     { 
-       System.err.println("Could not listen on port: 10008."); 
-       System.exit(1); 
-     } 
-     finally
-     {
-       try {
-         gui.serverSocket.close(); 
-       }
-       catch (IOException e)
-       { 
-         System.err.println("Could not close port: 10008."); 
-         System.exit(1); 
-       } 
-     }
-   }
- }
- 
+{
+    Server gui;
+    public Vector<PrintWriter> socketVector;
+    public Vector<Socket> socket;
+
+    public ConnectionThread (Server es3)
+    {
+        gui = es3;
+        socketVector = new Vector<>();
+        socket = new Vector<>();
+        start();
+    }
+
+    public void run()
+    {
+        gui.serverContinue = true;
+
+        try
+        {
+            gui.serverSocket = new ServerSocket(0);
+            gui.portInfo.setText("Listening on Port: " + gui.serverSocket.getLocalPort());
+            System.out.println ("Connection Socket Created");
+            try {
+                while (gui.serverContinue)
+                {
+                    System.out.println ("Waiting for Connection");
+                    gui.ssButton.setText("Stop Listening");
+                    CommunicationThread thread = new CommunicationThread (gui.serverSocket.accept(), gui, socketVector,socket);
+                    thread.start();
+                }
+            }
+            catch (IOException e)
+            {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+        }
+        catch (IOException e)
+        {
+            System.err.println("Could not listen on port: 10008.");
+            System.exit(1);
+        }
+        finally
+        {
+            try {
+                gui.serverSocket.close();
+            }
+            catch (IOException e)
+            {
+                System.err.println("Could not close port: 10008.");
+                System.exit(1);
+            }
+        }
+    }
+}
+
 
 class CommunicationThread extends Thread
-{ 
- //private boolean serverContinue = true;
- private Socket clientSocket;
- private Server gui;
+{
+    //private boolean serverContinue = true;
+    private Socket clientSocket;
+    private Server gui;
     public Vector<PrintWriter> socketVector;
+    public Vector<Socket> socket;
+
+
+    public CommunicationThread (Socket clientSoc, Server ec3, Vector<PrintWriter> socketVectorg, Vector<Socket> sock)
+    {
+        clientSocket = clientSoc;
+        sock.add(clientSocket);
+        this.socket = sock;
+        this.socketVector = socketVectorg;
+        //socketVector.add(clientSocket);
+        gui = ec3;
 
 
 
- public CommunicationThread (Socket clientSoc, Server ec3, Vector<PrintWriter> socketVectorg)
-   {
-    clientSocket = clientSoc;
-       //socketVectorg.add(clientSocket);
-       this.socketVector = socketVectorg;
-       //socketVector.add(clientSocket);
-    gui = ec3;
-   }
+    }
 
- public void run()
-   {
-    System.out.println ("New Communication Thread Started");
+    public void run()
+    {
+        System.out.println ("New Communication Thread Started");
 
-    try {
+        try {
 
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
                     true);
@@ -187,8 +187,16 @@ class CommunicationThread extends Thread
 
                 for(PrintWriter s : socketVector){
                     s.println(inputLine);
+
+                    for(Socket t : socket){
+                        int x = t.getPort();
+                        s.println(x);
+                    }
+
                 }
 
+
+                //out.println(inputLine);
 
                 if (inputLine.equals("Bye."))
                     break;
@@ -203,7 +211,6 @@ class CommunicationThread extends Thread
 
             }
 
-            System.out.println("GOT HERE HERE'S MY SOCKET:" + clientSocket);
             //Remove client from vector
             for( int i = 0; i < socketVector.size(); i++){
                 if(socketVector.elementAt(i) == out){
@@ -219,18 +226,12 @@ class CommunicationThread extends Thread
             System.out.println(socketVector.size());
 
 
-        } 
-    catch (IOException e) 
+        }
+        catch (IOException e)
         {
             //LOGOUT CLIENTS HERE (REMOVE FROM VECTOR)
-         System.err.println("Problem with Communication Server");
-         //System.exit(1); 
-        } 
+            System.err.println("Problem with Communication Server");
+            //System.exit(1);
+        }
     }
-} 
-
-
-
-
-
- 
+}
